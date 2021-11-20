a22632a1c31fb77668a522f213993de5823eba1a
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-11-20 17:27:30 +0200

    added forgotten file from the previous commit

--- src/pages/directory/directory.js
+++ src/pages/directory/directory.js
@@ -1,42 +1,45 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-import './directory.css';
-import '../../App.css';
+import "./directory.css";
+import "../../App.css";
 
-import Navbar from '../../components/navbar/navbar';
-import Product from '../../components/product/product';
-import Footer from '../../components/footer/footer';
+import Navbar from "../../components/navbar/navbar";
+import Product from "../../components/product/product";
+import Footer from "../../components/footer/footer";
 
-import { selectProducts } from '../../redux/directory/directory.selectors';
+import { selectProducts } from "../../redux/directory/directory.selectors";
 
 const Directory = ({ products }) => {
-	return (
-		<div className="App">
-			<div className="container-fluid mainHomePage">
-			<Navbar/>
-			
-			<section className="portfolio">
-				<div className="container">
-					<div className="portfolio-wrapper">
-					{ 
-						products.map( ({id, ...otherProps}) => (
-							<Product key={id} {...otherProps}/>
-						))
-					}
-					</div>
-				</div>
-				</section>
+  return (
+    <div className="App">
+      <div className="container-fluid mainHomePage">
+        <Navbar />
 
-			<Footer/>
-			</div>
-		</div>
-	);
-}
+        {/* <section className="portfolio">
+          <div className="container">
+            <div className="portfolio-wrapper"> */}
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {products.map(({ id, ...otherProps }) => (
+              <Product key={id} {...otherProps} />
+            ))}
+          </Masonry>
+        </ResponsiveMasonry>
+        {/* </div>
+          </div>
+        </section> */}
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-	products: selectProducts
+  products: selectProducts,
 });
 
 export default connect(mapStateToProps)(Directory);
