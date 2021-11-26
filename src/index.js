e502e03aa9d7b5a880bb4b54894ae796a6241b02
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-11-26 19:53:28 +0200

    removed everything to test what should be the fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor} from './redux/store.js';
+// import { store, persistor} from './redux/store.js';
 
 ReactDOM.render(
-  <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-  </Provider>,
+  <App/>,
+  // <Provider store={store}>
+  //     <PersistGate persistor={persistor}>
+  //       <App />
+  //     </PersistGate>
+  // </Provider>,
   document.getElementById('root')
 );
